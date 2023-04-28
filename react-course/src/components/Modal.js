2262e580b207fde3d73361840015293a44f89e36
Atif Patel <atifp89@gmail.com>
2023-04-28 11:27:55 +0100

    make use of useState to show Modal on delete

--- react-course/src/components/Modal.js
+++ react-course/src/components/Modal.js
@@ -2,8 +2,8 @@ function Modal() {
   return (
     <div className="modal">
       <p>Would you like to delete the card?</p>
-      <button className="btn">Confirm</button>
       <button className="btn btn--alt">Cancel</button>
+      <button className="btn">Confirm</button>
     </div>
   );
 }
--- react-course/src/components/Todo.js
+++ react-course/src/components/Todo.js
@@ -1,8 +1,19 @@
+import { useState } from "react";
+import Modal from "./Modal";
+import Backdrop from "./Backdrop";
+
 function Todo(params) {
+  // useState always returns an array with 2 elements, which can be stored in an array constant
+  // First element specifies the value of the current state
+  // Second value is a function that allows you to change the state
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
   //nested function created which is used for when the button 'Delete' is clicked
   function deleteHandler() {
+    // This now makes sure Modal is shown
+    setModalIsOpen(true);
     console.log("Button has been clicked!");
-    console.log("Card which was deleted: " + params.title);
+    console.log("Card which was chosen to be deleted: " + params.title);
   }
 
   return (
@@ -16,6 +27,11 @@ function Todo(params) {
           Delete
         </button>
       </div>
+      {
+        // Checks to see the state of modalIsOpen, if it is true, then the Modal and Backdrop functions are rendered
+      }
+      {modalIsOpen && <Modal />}
+      {modalIsOpen && <Backdrop />}
     </div>
   );
 }
